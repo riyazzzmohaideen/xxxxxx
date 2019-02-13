de5b80bd20df16d2b01a5dd814f4e6c7ebed02f1
riyazzzmohaideen <43335355+riyazzzmohaideen@users.noreply.github.com>
2019-02-13 14:55:22 +0530

    Create dasneee.c

---
+++ dasneee.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char c;
+int islowercasevowel1,isuppercasevowel1;
+printf("enter an alphabet:");
+scanf("%c",&c);
+islowercasevowel1=(c=='a'||c=='e'||c=='i'||c=='o'||c=="u");
+isuppercasevowel1=(c=='A'||c=='E'||c=='I'||c=='O'||c=="U");
+if(islowercasevowel||isuppercasevowel);
+printf("%c is a vowel",c);
+else
+printf("%c is a consonant",c);
+return 0;
+}
